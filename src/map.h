254f53971a22801bf546eeb0f629f14457b48483
CuiLily <cuili@shanghaitech.edu.cn>
2020-12-02 15:13:20 +0800

    can run until main-87row

--- src/map.h
+++ src/map.h
@@ -9,11 +9,10 @@ class Map {
 public:
 	
 	struct single_landmark_s{
-
-		std::string id_i ; // Landmark ID
-		double x_i; // Landmark x-position in the map (global coordinates)
-        double y_i; // Landmark y-position in the map (global coordinates)
-        double theta_i; //Landmark theta in the map (global coordinates)
+	    std::string id_i ; // Landmark ID
+	    double x_i; // Landmark x-position in the map (global coordinates)
+	    double y_i; // Landmark y-position in the map (global coordinates)
+	    double theta_i; //Landmark theta in the map (global coordinates)
 	};
 
 	std::vector<single_landmark_s> landmark_list ; // List of landmarks in the map
